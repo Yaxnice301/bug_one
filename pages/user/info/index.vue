9d08f242034dcdccf6316ee94cac48a6b0c394df
王雅萱 <yaxuan301@gmail.com>
2024-01-21 16:29:58 +0800

    add

--- pages/user/info/index.vue
+++ pages/user/info/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
     page :  /user/info
+
+    <div>
+      useRouter().currentRoute.value.path : {{useRouter().currentRoute.value.path}}
+    </div>
+     <div>
+      useRoute().path : {{useRoute().path}}
+    </div>
   </div>
 </template>
